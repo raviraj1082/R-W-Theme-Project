459bb2052deb98e060be16ae3190daad09977114
raviraj1082 <ravijethva3@gmail.com>
2021-09-07 14:46:51 +0530

    Search Filter

--- src/Containers/List.jsx
+++ src/Containers/List.jsx
@@ -3,24 +3,29 @@ import {Card,CardBody} from 'reactstrap';
 
 const List = (props) => {
     return (
-        <div className='col-3 pt-4'>
-            <Card>
-                <CardBody>
-                    {
-                        props.index !== undefined ? <h3>{props.index}</h3> : null
-                    }
-                    {
-                        props.name !== undefined ? <h4>{props.name}</h4> : null
-                    }
-                    {
-                        props.date !== undefined ? <h5>{props.date}</h5> : null
-                    }
-                    {
-                        props.desc !== undefined ? <p>{props.desc}</p> : null
-                    }
-                </CardBody>
-            </Card>
-        </div>
+        <>           
+            <div className='col-3 pt-4'>
+                <Card>
+                    <CardBody>
+                        {
+                            props.index !== undefined ? <h3>{props.index}</h3> : null
+                        }
+                        {
+                            props.name !== undefined ? <h4>Name: {props.name}</h4> : null
+                        }
+                        {
+                            props.name !== undefined ? <h5>Price: {props.price}</h5> : null
+                        }
+                        {
+                            props.date !== undefined ? <h6>Date: {props.date}</h6> : null
+                        }
+                        {
+                            props.desc !== undefined ? <p>Desc: {props.desc}</p> : null
+                        }
+                    </CardBody>
+                </Card>
+            </div>
+        </>
     );
 }
 
--- src/Containers/Medicines.jsx
+++ src/Containers/Medicines.jsx
@@ -1,50 +1,67 @@
 import React, { useState } from 'react';
+import { Input } from 'reactstrap';
 import List from './List';
 
 const medicineList =[
     {       
         name:'Ativan',
+        price:280.00,
         date:'28-10-2021',
         desc:'kidney or liver disease'
     },
     {
         name:'Entresto',
+        price:1080.00,
         date:'08-02-2021',
         desc:'liver disease/diabetes'
     },
     {
         name:'Ozempic',
+        price:987.00,
         date:'31-01-2023',
         desc:'low blood sugar'
     },
     {
         name:'Methotrexate',
+        price:364.00,
         date:'22-11-2022',
         desc:'low blood cell counts'
     },
     {
-        name:'Probuphine Implant',
+        name:'Probuphine ',
+        price:1245.00,
         date:'16-06-2021',
         desc:'mental illness or psychosis'
     },
     {
         name:'Cymbalta',
+        price:110.00,
         date:'14-03-2018',
         desc:'slow digestion'
     },
     {
         name:'Ativan',
+        price:280.00,
         date:'28-10-2021',
         desc:'kidney or liver disease'
     },
     {
         name:'Methotrexate',
+        price:364.00,
         date:'22-11-2022',
         desc:'low blood cell counts'
     }
 ]
 const Medicines = (props) => {
+
     const[medicine] = useState(medicineList)
+    const[search,setSearch] = useState('')
+    
+    let filterData = medicine.filter((val) => (val.name.toLowerCase().includes(search.toLowerCase()) || (val.price.toString().includes(search)) ))
+    
+    if(search === ''){
+        filterData = medicine
+    }
     return (
         <>  
             <main>
@@ -54,8 +71,13 @@ const Medicines = (props) => {
                             <h2>Medicine List</h2>
                         </div>
                         <div className="row">
+                        <div className="col-4 mx-auto">
+                            <Input type="text" placeholder='Search...' onChange={(e) => setSearch(e.target.value)} />
+                        </div>
+                        </div>
+                        <div className="row">                            
                             {
-                                medicine.map((val,index) => <List key={index+1} index={index + 1} name={val.name} date={val.date} desc={val.desc}/>)
+                                filterData.map((val,index) => <List key={index+1}  name={val.name} price={val.price} date={val.date} desc={val.desc}/>)
                             }
                         </div>
                     </div>
