b1f0e6d6faedcc2c013ecf4285ae041c37c77c59
raviraj1082 <ravijethva3@gmail.com>
2021-09-09 17:30:36 +0530

    newLogIn

--- src/Containers/Appointment.jsx
+++ src/Containers/Appointment.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 
 function Appointment(props) {
 
@@ -10,20 +10,116 @@ function Appointment(props) {
   const valGet = (e) =>{
     setNewVal( preVal => ({...preVal,[e.target.name]: e.target.value}))
   }
-  // console.log(preVal)
+  
   const validation = () =>{
+    let nameError = true
+    let emailError = true
+    let phoneError = true 
+    let dateError = true
+    let departmentError = true
+
     if(preVal.name !== undefined){
-     
+      if(preVal.name === ''){
+        setError(error => ({...error, name:'Enter your name'})) 
+      }else{
+        var validName = /^[a-zA-Z ]+$/
+        if(validName.test(preVal.name) === false){
+          setError(error => ({...error, name:'Please enter valid name'}))
+        }else{
+          setError(error => ({...error, name:''}))
+          nameError = false
+        }
+      }           
+    }
+    if(preVal.email !== undefined){
+      if(preVal.email === ''){
+        setError(error => ({...error, email:'Enter your email'})) 
+      }else{
+        var valEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+        if(valEmail.test(preVal.email) === false){
+          setError(error => ({...error, email:'Please enter valid email'}))
+        }else{
+          setError(error => ({...error, email:''}))
+          emailError = false
+        }
+      }           
+    }
+    if(preVal.phone !== undefined){
+      if(preVal.phone === ''){
+        setError(error => ({...error, phone:'Enter your phone number'})) 
+      }else{
+        var valphone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+        if(valphone.test(preVal.phone) === false){
+          setError(error => ({...error, phone:'Please valid phone number'}))
+        }else{
+          setError(error => ({...error, phone:''}))
+          phoneError = false
+        }
+      }           
+    }
+    if(preVal.date !== undefined){
+      if(preVal.date === ''){
+        setError(error => ({...error, date:'Enter your date'})) 
+      }else{
+        var valdate = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
+        if(valdate.test(preVal.date) === false){
+          setError(error => ({...error, date:'Please valid date'}))
+        }else{
+          setError(error => ({...error, date:''}))
+          dateError = false
+        }
+      }           
+    }
+    if(preVal.department !== undefined){
+      if(preVal.department === '0'){
+        setError(error => ({...error, department:'Select your department'})) 
+      }else{
+        setError(error => ({...error, department:''}))
+        departmentError = false
+      }        
+    }
+    if((nameError || emailError || phoneError || dateError || departmentError) === true){
+      return false
+    }else{
+      return true
     }
   }
+  useEffect(() => {
+    validation()
+  }, [preVal]) 
+  
   const subitForm = (e) =>{
+    
     e.preventDefault()
-    let isValid = validation()
+
+    let isValid = true
+    isValid = validation()
+    
+    if(preVal.name === undefined){
+      setError(error=> ({...error, name:'Enter your name'}))
+      isValid = false 
+    }
+    if(preVal.email === undefined){
+      setError(error=> ({...error, email:'Enter your email'})) 
+      isValid = false
+    }
+    if(preVal.phone === undefined){
+      setError(error=> ({...error, phone:'Enter your phone number'}))
+      isValid = false 
+    }
+    if(preVal.date === undefined){
+      setError(error=> ({...error, date:'Enter your date'}))
+      isValid = false 
+    }
+    if(preVal.department === undefined){
+      setError(error=> ({...error, department:'Enter your department'}))
+      isValid = false 
+    }
     
     if(isValid){
-      console.log('validation')
+      return true
     }else{
-
+      return false
     }
   }
   return (      
@@ -39,59 +135,63 @@ function Appointment(props) {
             </div>
             <form onSubmit={(e) => subitForm(e)} className="php-email-form" method="post">
               <div className="row">
-                <div className="col-md-4 form-group">
+                <div className="col-md-4 form-group pb-3">
                   <input type="text" 
                     name="name" 
                     className="form-control" 
                     id="name" 
                     placeholder="Your Name" 
+                    autoComplete="off"
                     onChange={(e) => valGet(e)}
                   />
-                  <small style={{color:'red'}}>{error.name !== undefined ? error.name : ''}</small>
+                  <small style={{color:'red',position:'absolute'}}>{error.name !== undefined ? error.name : ''}</small>
                 </div>
-                <div className="col-md-4 form-group mt-3 mt-md-0">
+                <div className="col-md-4 form-group mt-3 mt-md-0 pb-3">
                   <input 
                    type="email"
                    className="form-control"
                    name="email" 
                    id="email" 
                    placeholder="Your Email" 
+                   autoComplete="off"
                    onChange={(e) => valGet(e)}
                    />
-                  <small style={{color:'red'}}>{error.name !== undefined ? error.name : ''}</small>
+                  <small style={{color:'red',position:'absolute'}}>{error.email !== undefined ? error.email : ''}</small>
                 </div>
-                <div className="col-md-4 form-group mt-3 mt-md-0">
+                <div className="col-md-4 form-group mt-3 mt-md-0 pb-3">
                   <input 
                   type="tel" 
                   className="form-control" 
                   name="phone" id="phone" 
                   placeholder="Your Phone" 
                   data-rule="minlen:4" 
+                  autoComplete="off"
                   onChange={(e) => valGet(e)}
                  />
-                 <small style={{color:'red'}}>{error.name !== undefined ? error.name : ''}</small>
+                 <small style={{color:'red',position:'absolute'}}>{error.phone !== undefined ? error.phone : ''}</small>
                 </div>
               </div>
               <div className="row">
-                <div className="col-md-4 form-group mt-3">
+                <div className="col-md-4 form-group mt-3 pb-3">
                   <input 
                   type="datetime" 
                   name="date" 
                   className="form-control datepicker" 
                   id="date" 
+                  autoComplete="off"
                   placeholder="Appointment Date" 
                   onChange={(e) => valGet(e)}
                   />
-                  <small style={{color:'red'}}>{error.name !== undefined ? error.name : ''}</small>
+                  <small style={{color:'red',position:'absolute'}}>{error.date !== undefined ? error.date : ''}</small>
                 </div>
-                <div className="col-md-4 form-group mt-3">
+                <div className="col-md-4 form-group mt-3 pb-3">
                   <select name="department" id="department" className="form-select"  onChange={(e) => valGet(e)}>
                     <option value="0">Select Department</option>
                     <option value="ReactJs">ReactJs</option>
                     <option value="Php">Php</option>
                     <option value="Graphic">Graphic</option>
                   </select>
-                  <div className="validate"></div>
+                  <small style={{color:'red',position:'absolute'}}>{error.department !== undefined ? error.department : ''}</small>
                 </div>
               </div>
               <div className="form-group mt-3">
