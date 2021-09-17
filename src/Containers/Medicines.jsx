ebb31fe193e5fc618b72ba338e3342f2e49abdfa
raviraj1082 <ravijethva3@gmail.com>
2021-09-17 12:37:40 +0530

    LiveSite

--- src/Containers/Medicines.jsx
+++ src/Containers/Medicines.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import List from './List';
 import {Input } from 'reactstrap';
 import Addmedicine from './Addmedicine';
